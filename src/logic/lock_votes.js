823bf5633f1221543085416d38e3caa30908edb6
SyAtlantis <1102960695@qq.com>
2018-09-26 15:42:42 +0800

    lock and unlock

--- src/logic/lock_votes.js
+++ src/logic/lock_votes.js
@@ -1,54 +1,79 @@
 "use strict";
 
 function LockVotes() {
-    this.create = function (data, trs) {
-        trs.args = data.args;
-        return trs;
+  this.create = function (data, trs) {
+    trs.args = data.args;
+    return trs;
+  }
+
+  this.calculateFee = function (trs, sender) {
+    return library.base.block.calculateFee();
+  }
+
+  this.verify = function (trs, sender, cb) {
+    let amount = Number(trs.args[0]);
+    if(amount < 0){
+      return cb('Invalid lock amount!');
     }
 
-    this.calculateFee = function (trs, sender) {
-        return library.base.block.calculateFee();
+    let fee = this.calculateFee(trs, sender);
+    if(sender.balance < amount + fee){
+      return cb('Not enough balance');
     }
 
-    this.verify = function (trs, sender, cb) {
-        // TODO
+    cb(null, trs);
+  }
+
+  this.process = function (trs, sender, cb) {
+    setImmediate(cb, null, trs);
+  }
+
+  this.getBytes = function (trs) {
+    return null;
+  }
+
+  this.apply = function (trs, block, sender, cb) {
+    // TODO
+  }
+
+  this.undo = function (trs, block, sender, cb) {
+    // TODO
+  }
+
+  this.applyUnconfirmed = function (trs, sender, cb) {
+    var key = sender.address + ':' + trs.type
+    if (library.oneoff.has(key)) {
+      return setImmediate(cb, 'Double submit')
+    }
+    library.oneoff.set(key, true)
+    setImmediate(cb)
+  }
+
+  this.undeUnconfirmed = function (trs, sender, cb) {
+    var key = sender.address + ':' + trs.type
+    library.oneoff.delete(key)
+    setImmediate(cb)
+  }
+
+  this.objectNormalize = function (trs) {
+    return trs;
+  }
+
+  this.dbRead = function (raw) {
+    // TODO
+  }
+
+  this.ready = function (trs, sender) {
+    if (!__private.types[trs.type]) {
+      throw Error('Unknown transaction type ' + trs.type);
     }
 
-    this.process = function (trs, sender, cb) {
-        setImmediate(cb, null, trs);
+    if (!sender) {
+      return false;
     }
 
-    this.getBytes = function (trs) {
-        return null;
-    }
-
-    this.apply = function (trs, block, sender, cb) {
-        // TODO
-    }
-
-    this.undo = function (trs, block, sender, cb) {
-        // TODO
-    }
-
-    this.applyUnconfirmed = function (trs, sender, cb) {
-        // TODO
-    }
-
-    this.undeUnconfirmed = function (trs, sender, cb) {
-        // TODO
-    }
-
-    this.objectNormalize = function (trs) {
-        // TODO
-    }
-
-    this.dbRead = function (raw) {
-        // TODO
-    }
-
-    this.ready = function (trs, sender) {
-        // TODO
-    }
+    return __private.types[trs.type].ready.call(this, trs, sender);
+  }
 
 }
 
--- src/logic/unlock_votes.js
+++ src/logic/unlock_votes.js
@@ -1,54 +1,74 @@
 "use strict";
 
 function UnlockVotes() {
-    this.create = function (data, trs) {
-        trs.args = data.args;
-        return trs;
+  this.create = function (data, trs) {
+    trs.args = data.args;
+    return trs;
+  }
+
+  this.calculateFee = function (trs, sender) {
+    return library.base.block.calculateFee();
+  }
+
+  this.verify = function (trs, sender, cb) {
+    let ids = trs.args;
+    if(ids.length < 0){
+      return cb('Invalid unlock transactions id!');
     }
 
-    this.calculateFee = function (trs, sender) {
-        return library.base.block.calculateFee();
-    }
+    cb(null, trs);
+  }
 
-    this.verify = function (trs, sender, cb) {
-        // TODO
-    }
+  this.process = function (trs, sender, cb) {
+    setImmediate(cb, null, trs);
+  }
 
-    this.process = function (trs, sender, cb) {
-        setImmediate(cb, null, trs);
-    }
+  this.getBytes = function (trs) {
+    return null;
+  }
 
-    this.getBytes = function (trs) {
-        return null;
-    }
+  this.apply = function (trs, block, sender, cb) {
+    // TODO
+  }
 
-    this.apply = function (trs, block, sender, cb) {
-        // TODO
-    }
+  this.undo = function (trs, block, sender, cb) {
+    // TODO
+  }
 
-    this.undo = function (trs, block, sender, cb) {
-        // TODO
+  this.applyUnconfirmed = function (trs, sender, cb) {
+    var key = sender.address + ':' + trs.type
+    if (library.oneoff.has(key)) {
+      return setImmediate(cb, 'Double submit')
     }
+    library.oneoff.set(key, true)
+    setImmediate(cb)
+  }
 
-    this.applyUnconfirmed = function (trs, sender, cb) {
-        // TODO
-    }
+  this.undeUnconfirmed = function (trs, sender, cb) {
+    var key = sender.address + ':' + trs.type
+    library.oneoff.delete(key)
+    setImmediate(cb)
+  }
 
-    this.undeUnconfirmed = function (trs, sender, cb) {
-        // TODO
-    }
+  this.objectNormalize = function (trs) {
+    // TODO
+  }
 
-    this.objectNormalize = function (trs) {
-        // TODO
-    }
+  this.dbRead = function (raw) {
+    // TODO
+  }
 
-    this.dbRead = function (raw) {
-        // TODO
-    }
+  this.ready = function (trs, sender) {
+    if (sender.multisignatures.length) {
+      if (!trs.signatures) {
+        return false;
+      }
 
-    this.ready = function (trs, sender) {
-        // TODO
+      return trs.signatures.length >= sender.multimin - 1;
+    } else {
+      return true;
     }
+  }
 
 }
 
