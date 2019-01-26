bd97946961a14ac7ea0a7ec83281f286d0806b75
alpha-entanmo <alpha.entanmo@gmail.com>
2019-01-26 13:46:18 +0800

    fixedme

--- src/modules/delegates.js
+++ src/modules/delegates.js
@@ -716,7 +716,7 @@ __private.loop = function (cb) {
     return setImmediate(cb);
   }
 
-  if (Date.now() % 500 > 300) {
+  if (Date.now() % 3000 > 2000) {
     library.logger.trace('Loop:', 'maybe too late to collect votes');
     return setImmediate(cb);
   }
